17b947006359498cd411b995501c8cd52f750339
asafdari-boop <asafdari@umich.edu>
2024-02-29 23:56:51 -0800

    add few shot kwargs param to use msgs as few shot user assistant pairs

--- examples/example.py
+++ examples/example.py
@@ -7,10 +7,11 @@ thinker_prompt = prompts['thinker_prompt']
 final_prompt = prompts['final_prompt']
 
 def QAchain(context, question):
+    msgs = [{"role": "user","content": "blah"},{"role": "assistant","content": "blah"} ]
     system_input={'name': 'Claros'}
     user_input = {'context': context, 'question': question}
-    reasoning = thinker_prompt(system_inputs=system_input, user_inputs=user_input, debug=True)
-    
+    reasoning = thinker_prompt(system_inputs=system_input, user_inputs=user_input, messages=msgs, few_shot=True, debug=True)
+
     user_input = {'question': question, 'reasoning': reasoning}
     out = final_prompt(user_input, debug=True)
     return out
