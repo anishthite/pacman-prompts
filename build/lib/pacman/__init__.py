337a909896e296f400327745e6c95680033097ba
Anish Thite <anishthite@gmail.com>
2024-04-29 13:10:20 -0700

    clean repo

--- build/lib/pacman/__init__.py
+++
@@ -1,2 +0,0 @@
-from pacman.pacman import *
-from pacman.prompt import *
--- build/lib/pacman/file_utils.py
+++
@@ -1,6 +0,0 @@
-from yaml import load, Loader
-
-def load_yml(path):
-    with open(path, "r") as f:
-        return load(f, Loader=Loader)
-
